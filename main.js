6507b9b986fecc0317358d09512741040446c6f0
FernandaMG <51218735+FernandaMG@users.noreply.github.com>
2019-05-31 12:55:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+if (tgt == atras) {
+				if (cont > 0) {
+					img.src = imagenes [cont - 1];
+					cont--;
+				} else {
+					img.src = imagenes[imagenes.length - 1];
+					cont = imagenes.length - 1;
+				}
+
+			} else if (tgt == adelante) {
+				if (cont < imagenes.length - 1) {
+					img.src = imagenes[cont + 1];
+					cont++;
+				}else {
+					img.src = imagenes[0];
+					cont = 0;
+				}
+			}	
